ad92b19f87960d650d5b743a16ecbbfc53678ef2
Herman Chenwi <hermanchenwi@gmail.com>
2019-06-16 12:19:37 -0400

    totally changed the work I did but maintained the grid and text the submit and return function to see if it works.text was a soso success

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//function btn001(){
+  //var text001;
+  //var colors001 = input001.value;
+  //if (colors001 == "blue") {
+    //text001 = "the color is blue";
+    //document.getElementById("message001").innerHTML = text001;
+//  }
+//if (colors001 == "yellow") {
+  //text001 = "The color is yellow";
+  //document.getElementById("message001").innerHTML = text001;
+//}
+//}
